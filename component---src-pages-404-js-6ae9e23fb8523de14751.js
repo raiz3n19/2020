357e86fdd981498441558f55f9820d4d97483d8c
raiz3n19 <raiz3n19@users.noreply.github.com>
2020-10-06 21:22:11 +0000

    deployed via Gatsby Publish Action 🎩 for f77bcd336257a3e9429e0deb52b68747af36330c

---
+++ component---src-pages-404-js-6ae9e23fb8523de14751.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("Bl7J"),r=t("vrFN");n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"[ 404 NOT FOUND ]"}),l.a.createElement("h1",null,"Soo Sadness..."),l.a.createElement("p",null,"We Could Not Find The Above Page From Our Website ."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6ae9e23fb8523de14751.js.map
